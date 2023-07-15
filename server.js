cd818638ddb996613da24a09a756e312a586fbc6
Narayan Aryal <aryal.coding@gmail.com>
2023-07-15 18:04:57 +1000

    code modefied

--- server.js
+++ server.js
@@ -1,60 +1,51 @@
-import express from 'express'
-import morgan from 'morgan'
-import cors from 'cors'
-import dotenv from 'dotenv'
-dotenv.config()
-import { connectDb } from './src/config/configDb.js'
-import UserRouter from './src/router/UserRouter.js'
+import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+import { connectDb } from "./src/config/configDb.js";
+import UserRouter from "./src/router/UserRouter.js";
 
-const app = express()
+const app = express();
 
-const PORT = 8000
+const PORT = 9000;
 
 //middlewares
-app.use(express.json())
-app.use(cors())
-app.use(morgan("dev"))
+app.use(express.json());
+app.use(cors());
+app.use(morgan("dev"));
 
 //database
-connectDb()
-
+connectDb();
 
 //router
-app.use("/api/v1/user", UserRouter)
-
+app.use("/api/v1/user", UserRouter);
 
 //uncaught error handler
 
 app.use("*", (req, res, next) => {
-    try {
-        const error ={
-            errorCode: 404,
-            message: error.message}
-
-    } catch (error) {
-        next(error)
-    }
-    
-  
-
-
-}
-)
-
-
-
+  try {
+    const error = {
+      errorCode: 404,
+      message: error.message,
+    };
+  } catch (error) {
+    next(error);
+  }
+});
 
 //global error handler
-app.use((error, req, res, next)=> {
-   const statusCode = error.errorCode || 404
+app.use((error, req, res, next) => {
+  const statusCode = error.errorCode || 404;
 
-   res.status(statusCode).json({
-    status:"error",
-    message: error.message
-   })
-})
+  res.status(statusCode).json({
+    status: "error",
+    message: error.message,
+  });
+});
 
-
-app.listen(PORT, (error)=> {
-    error ? console.log(error) : console.log(`your server is running at http://localhost:${PORT}`)
-})
+app.listen(PORT, (error) => {
+  error
+    ? console.log(error)
+    : console.log(`your server is running at http://localhost:${PORT}`);
+});
